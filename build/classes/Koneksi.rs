8c7bce2ae28febea126760a994c66ba8d6ce6d8e
Your Name <you@example.com>
2026-01-04 17:07:14 +0700

    Upload source code saja tanpa folder dist dan file besar

--- build/classes/Koneksi.rs
+++ build/classes/Koneksi.rs
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 aplikasiarisan.Koneksi
+=======
+aplikasiarisan.Koneksi
+>>>>>>> 51db8fd308bbcfc1317543e5a9739cdbe0652936
--- build/classes/MainFrame.rs
+++ build/classes/MainFrame.rs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 aplikasiarisan.MainFrame$1
 aplikasiarisan.MainFrame$10
 aplikasiarisan.MainFrame$11
@@ -10,3 +11,17 @@ aplikasiarisan.MainFrame$9
 aplikasiarisan.MainFrame$6
 aplikasiarisan.MainFrame$7
 aplikasiarisan.MainFrame
+=======
+aplikasiarisan.MainFrame$1
+aplikasiarisan.MainFrame$10
+aplikasiarisan.MainFrame$11
+aplikasiarisan.MainFrame$4
+aplikasiarisan.MainFrame$5
+aplikasiarisan.MainFrame$2
+aplikasiarisan.MainFrame$3
+aplikasiarisan.MainFrame$8
+aplikasiarisan.MainFrame$9
+aplikasiarisan.MainFrame$6
+aplikasiarisan.MainFrame$7
+aplikasiarisan.MainFrame
+>>>>>>> 51db8fd308bbcfc1317543e5a9739cdbe0652936
